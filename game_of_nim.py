953461b83be4b47cd4411b430c857ab361efa602
super-rogatory <chukwudi10@gmail.com>
2022-04-13 23:13:14 -0700

    feature: tentative actions, utility, and terminal_test

--- game_of_nim.py
+++ game_of_nim.py
@@ -13,25 +13,23 @@ from games import *
 
 
 class GameOfNim(Game):
-    # YOUR CODE GOES HERE
     def __init__(self, board):
         # Similar to TicTacToe, we can define the parameters of our problem.
         moves = []
         for i in range(len(board)):
             for j in range(1, board[i] + 1):  # ex.) changed from [0,5) to [1,6)
                 moves.append((i, j))
-        self.initial = GameState(to_move="MAX", utility=0, board=board, moves=moves)
+        self.initial = GameState(to_move="MAX", utility=1, board=board, moves=moves)
 
     def result(self, state, move):
         if move not in state.moves:
             return state
         board = state.board.copy()  # creates copy of the board
-        # modifies the board in response to move
         (index, amount_to_remove) = move  # unpacks the index and the amount to remove
         board[index] -= amount_to_remove  # removes amount from element at position
-        moves = []
+        moves = []  # create a new move list with new bounds
         for i in range(len(board)):
-            for j in range(1, board[i] + 1):  # ex.) changed from [0,5) to [1,6)
+            for j in range(1, board[i] + 1):
                 moves.append((i, j))
         return GameState(
             to_move=("MAX" if state.to_move == "MIN" else "MIN"),
@@ -39,6 +37,18 @@ class GameOfNim(Game):
             board=board,
             moves=moves,
         )
+
+    def actions(self, state):
+        # return moves list
+        return state.moves
+
+    def terminal_test(self, state):
+        # returns true if no more moves left or if the board is all zeroes
+        return True if len(state.moves) == 0 or sum(state.board) == 0 else False
+
+    def utility(self, state, player):
+        # if "MAX" wins return 1, else return -1
+        return state.utility if player == "MAX" else -state.utility
 
 
 if __name__ == "__main__":
